e36f0adf14d2de0c3e6627b4e574bd1e8e5fadec
pasha55557 <al.paha@yandex.ru>
2020-02-04 18:05:13 +0300

    all parse

--- includes/pars_obj.h
+++ includes/pars_obj.h
@@ -6,49 +6,52 @@
 /*   By: rsticks <rsticks@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/30 15:36:04 by rsticks           #+#    #+#             */
-/*   Updated: 2020/02/03 20:01:15 by rsticks          ###   ########.fr       */
+/*   Updated: 2020/02/04 16:14:39 by rsticks          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARS_OBJ
 # define PARS_OBJ
 
-typedef struct 			s_list_vn
+typedef struct 			s_int_vec
 {
 	int					x;
 	int					y;
 	int					z;
-}						t_list_vn;
+}						t_int_vec;
+
+typedef struct 			s_list_vt
+{
+	cl_float			u;
+	cl_float			v;
+}						t_list_vt;
 
 typedef struct			s_list_v
 {
-	float				x;
-	float				y;
-	float				z;
+	cl_float3			v;
 }						t_list_v;
 
-typedef struct			s_cl_obj
+typedef struct			s_list_f
 {
-	cl_int3				f;
-	cl_float3			v1;
-	cl_float3			v2;
-	cl_float3			v3;
-	cl_int				num_obj;		
-}						t_cl_obj;
+	cl_int3				v;
+	cl_int3				vn;
+	cl_int3				vt;
+}						t_list_f;
 
 typedef struct 			s_parse_obj
 {
 	int					num_obj;
 	char				*name;
+	u_int32_t			count_vt;
 	u_int32_t			count_v;
 	u_int32_t			count_vn;
 	u_int32_t			count_f;
 	int					fd;
 	char				*line;
-	t_list_v			*v;
-	t_list_vn			*vn;
-	int					**f;
-	t_cl_obj			*obj;
+	cl_float3			*v;
+	cl_float3			*vn;
+	t_list_vt			*vt;
+	t_list_f			*f;
 }						t_parse_obj;
 
 void					parsing_obj(char *path, int num);
--- source/parser_obj/parse_obj.c
+++ source/parser_obj/parse_obj.c
@@ -6,7 +6,7 @@
 /*   By: rsticks <rsticks@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 18:53:55 by rsticks           #+#    #+#             */
-/*   Updated: 2020/02/03 19:56:53 by rsticks          ###   ########.fr       */
+/*   Updated: 2020/02/04 18:02:21 by rsticks          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,41 @@ static char		*next_digit(char *c, int *error)
 	return (c);
 }
 
+
 void			parse_v(t_parse_obj *data)
+{
+	char		*c;
+	u_int32_t	count;
+	int			error;
+	t_vector	v;
+
+	error = 0;
+	count = 0;
+	if (!(data->v = (cl_float3*)malloc(sizeof(cl_float3) * data->count_v)))
+		kill_all("Can't alloc memmory to data->v\n");
+	while (count != data->count_v)
+	{
+		if (ft_strstr(data->line, "v "))
+		{
+			c = data->line;
+			c = while_not_digit(c, &error);
+			v.x = ft_atof(c);
+			c = next_digit(c, &error);
+			v.y = ft_atof(c);
+			c = next_digit(c, &error);
+			v.z = ft_atof(c);
+			data->v[count] = (cl_float3){{v.x, v.y, v.z}};
+			if (error)
+				kill_all(ft_strjoin("Wrong file: ", data->name));
+		}
+		if (count != (data->count_v - 1))
+			get_next_line(data->fd, &data->line);
+		count++;
+	}
+		free(data->line);
+}
+
+void			parse_vt(t_parse_obj *data)
 {
 	char		*c;
 	u_int32_t	count;
@@ -70,26 +104,22 @@ void			parse_v(t_parse_obj *data)
 
 	error = 0;
 	count = 0;
-	if (!(data->v = (t_list_v*)malloc(sizeof(t_list_v) * data->count_v)))
-		kill_all("Can't alloc memmory to data->v\n");
-	get_next_line(data->fd, &data->line);
-	while (!(ft_strstr(data->line, "v ")))
-		get_next_line(data->fd, &data->line);
-	while (count != data->count_v)
+	if (!(data->vt = (t_list_vt*)malloc(sizeof(t_list_vt) * data->count_vt)))
+		kill_all("Can't alloc memmory to data->vn\n");
+	while (count != data->count_vt)
 	{
-		if (ft_strstr(data->line, "v "))
+		if (ft_strstr(data->line, "vt "))
 		{
 			c = data->line;
 			c = while_not_digit(c, &error);
-			data->v[count].x = ft_atof(c);
+			data->vt[count].u = ft_atof(c);
 			c = next_digit(c, &error);
-			data->v[count].y = ft_atof(c);
-			c = next_digit(c, &error);
-			data->v[count].z = ft_atof(c);
+			data->vt[count].v = ft_atof(c);
 			if (error)
 				kill_all(ft_strjoin("Wrong file: ", data->name));
 		}
-		get_next_line(data->fd, &data->line);
+		if (count != (data->count_vt - 1))
+			get_next_line(data->fd, &data->line);
 		count++;
 	}
 		free(data->line);
@@ -100,31 +130,105 @@ void			parse_vn(t_parse_obj *data)
 	char		*c;
 	u_int32_t	count;
 	int			error;
+	t_vector	vn;
 
-	error = 1;
+	error = 0;
 	count = 0;
-	if (!(data->vn = (t_list_vn*)malloc(sizeof(t_list_vn) * data->count_vn)))
+	if (!(data->vn = (cl_float3*)malloc(sizeof(cl_float3) * data->count_vn)))
 		kill_all("Can't alloc memmory to data->vn\n");
-	while (!(ft_strstr(data->line, "vn ")))
-		get_next_line(data->fd, &data->line);
 	while (count != data->count_vn)
 	{
-		if (ft_strstr(data->line, "v "))
+		if (ft_strstr(data->line, "vn "))
 		{
 			c = data->line;
 			c = while_not_digit(c, &error);
-			data->vn[count].x = ft_atoi(c);
+			vn.x = ft_atof(c);
 			c = next_digit(c, &error);
-			data->vn[count].y = ft_atoi(c);
+			vn.y = ft_atof(c);
 			c = next_digit(c, &error);
-			data->vn[count].z = ft_atoi(c);
+			vn.z = ft_atof(c);
+			data->vn[count] = (cl_float3){{vn.x, vn.y, vn.z}};
 			if (error)
 				kill_all(ft_strjoin("Wrong file: ", data->name));
 		}
-		get_next_line(data->fd, &data->line);
+		if (count != (data->count_vn - 1))
+			get_next_line(data->fd, &data->line);
 		count++;
 	}
 		free(data->line);
+}
+
+int				while_not_slash(char *c)
+{
+	while (*c != '/' && *c != ' ')
+		c++;
+	if (*c == '/')
+	{
+		c++;
+		return (1);
+	}
+	else
+		return (0);
+}
+
+char			*f_pars_x(t_parse_obj *data, char *c, u_int32_t count)
+{
+	t_int_vec	x;
+
+	x.x = 0;
+	x.y = 0;
+	x.z = 0;
+	x.x = ft_atoi(c);
+	if (!while_not_slash(c))
+		return (c);
+	x.y = ft_atoi(c);
+	if (!while_not_slash(c))
+		return (c);
+	x.z = ft_atoi(c);
+	data->f[count].v.x = x.x;
+	data->f[count].vt.x = x.y;
+	data->f[count].vn.x = x.z;
+	return (c);
+}
+
+char			*f_pars_y(t_parse_obj *data, char *c, u_int32_t count)
+{
+	t_int_vec	y;
+
+	y.x = 0;
+	y.y = 0;
+	y.z = 0;
+	y.x = ft_atoi(c);
+	if (!while_not_slash(c))
+		return (c);
+	y.y = ft_atoi(c);
+	if (!while_not_slash(c))
+		return (c);
+	y.z = ft_atoi(c);
+	data->f[count].v.y = y.x;
+	data->f[count].vt.y = y.y;
+	data->f[count].vn.y = y.z;
+	return (c);
+}
+
+char			*f_pars_z(t_parse_obj *data, char *c, u_int32_t count)
+{
+	t_int_vec	z;
+
+	z.x = 0;
+	z.y = 0;
+	z.z = 0;
+	z.x = ft_atoi(c);
+	if (!while_not_slash(c))
+		return (c);
+	z.y = ft_atoi(c);
+	if (!while_not_slash(c))
+		return (c);
+	z.z = ft_atoi(c);
+	data->f[count].v.z = z.x;
+	data->f[count].vt.z = z.y;
+	data->f[count].vn.z = z.z;
+	return (c);
 }
 
 void			final_parse(t_parse_obj *data)
@@ -132,40 +236,34 @@ void			final_parse(t_parse_obj *data)
 	char		*c;
 	int			error;
 	u_int32_t	count;
-	t_list_vn	f;
 
 	error = 0;
 	count = 0;
-	if (!(data->obj = (t_cl_obj*)malloc(sizeof(t_cl_obj) * data->count_f)))
+	if (!(data->f = (t_list_f*)malloc(sizeof(t_list_f) * data->count_f)))
 		kill_all("Can't alloc memmory to data->obj\n");
-	while (!(ft_strstr(data->line, "f ")))
-		get_next_line(data->fd, &data->line);
 	while (count != data->count_f)
 	{
 		if (ft_strstr(data->line, "f "))
 		{
 			c = data->line;
 			c = while_not_digit(c, &error);
-			f.x = ft_atoi(c);
+			c = f_pars_x(data, c, count);
 			c = next_digit(c, &error);
-			f.y = ft_atoi(c);
+			c = f_pars_y(data, c, count);
 			c = next_digit(c, &error);
-			f.z = ft_atoi(c);
-			data->obj[count].f = (cl_int3){{f.x, f.y, f.z}};
-			data->obj[count].v1 = (cl_float3){{data->v[f.x - 1].x, data->v[f.x - 1].y, data->v[f.x - 1].z}};
-			data->obj[count].v2 = (cl_float3){{data->v[f.y - 1].x, data->v[f.y - 1].y, data->v[f.y - 1].z}};
-			data->obj[count].v3 = (cl_float3){{data->v[f.z - 1].x, data->v[f.z - 1].y, data->v[f.z - 1].z}};
-			data->obj[count].num_obj = data->num_obj;
+			c = f_pars_z(data, c, count);
 		}
+		if (count != (data->count_f - 1))
 		get_next_line(data->fd, &data->line);
 		count++;
 	}
-		free(data->line);
+	free(data->line);
 }
 
 static void		init_data(t_parse_obj *data, int num)
 {
 	data->num_obj = num;
+	data->count_vt = 0;
 	data->count_v = 0;
 	data->count_vn = 0;
 	data->count_f = 0;
@@ -174,7 +272,9 @@ static void		init_data(t_parse_obj *data, int num)
 void			parsing_obj(char *path, int num)
 {
 	t_parse_obj *data;
+	int			gnl;
 
+	gnl = 1;
 	if (!(data = (t_parse_obj*)malloc(sizeof(t_parse_obj))))
 		kill_all("Can't initialize obj_data\n");
 	init_data(data, num);
@@ -182,21 +282,29 @@ void			parsing_obj(char *path, int num)
 	if (!(data->fd = open(path, O_RDONLY)))
 		kill_all("Can't open obj file\n");
 	counter(data, "v ", &data->count_v);
-	counter(data, "vn ", &data->count_vn);
-	counter(data, "f ", &data->count_f);
+	while (gnl)
+	{
+		if (ft_strstr(data->line, "vn "))
+			counter(data, "vn ", &data->count_vn);
+		else if (ft_strstr(data->line, "vt "))
+			counter(data, "vt ", &data->count_vt);
+		else if (ft_strstr(data->line, "f "))
+			counter(data, "f ", &data->count_f);
+		else
+			gnl = get_next_line(data->fd, &data->line);
+	}
 	close(data->fd);
 	data->fd = open(path, O_RDONLY);
-	parse_v(data);
-	final_parse(data);
-	u_int32_t count;
-	count = 0;
-
-	while (count != data->count_f)
+	while (get_next_line(data->fd, &data->line))
 	{
-		printf("f %d (x: %-10f y: %-10f z: %-10f) || %d (x: %-10f y: %-10f z: %-10f) || %d (x: %-10f y: %-10f z: %-10f)\n", 
-			data->obj[count].f.x, data->obj[count].v1.x, data->obj[count].v1.y, data->obj[count].v1.z,
-			data->obj[count].f.y, data->obj[count].v2.x, data->obj[count].v2.y, data->obj[count].v2.z,
-			data->obj[count].f.z, data->obj[count].v3.x, data->obj[count].v3.y, data->obj[count].v3.z);
-		count++;
-	}	
+		if (ft_strstr(data->line, "v "))
+			parse_v(data);
+		else if (ft_strstr(data->line, "vn "))
+			parse_vn(data);
+		else if (ft_strstr(data->line, "vt "))
+			parse_vt(data);
+		else if (ft_strstr(data->line, "f "))
+			final_parse(data);	
+	}
+	close(data->fd);
 }
