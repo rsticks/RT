7e53377c2c7fedf2cbcf4878ee21192f7473f9e1
daron <slava.barabanov2010@yandex.ru>
2020-02-09 15:47:10 +0300

    fix sega

--- new_parser/source/parser/object_parser.c
+++ new_parser/source/parser/object_parser.c
@@ -69,6 +69,16 @@ void check_inti_obj_type(t_rt *rt, int str_c)
 		kill_error("uncorrected object type" , str_c);
 }
 
+void check_inti_obj_rgb(t_rt *rt, int str_c)
+{
+	if (!(rt->obj_cur->rgb.r >= 0 && rt->obj_cur->rgb.r <= 255))
+		kill_error("Wrong red color component", str_c);
+	else if (!(rt->obj_cur->rgb.g >= 0 && rt->obj_cur->rgb.g <= 255))
+		kill_error("Wrong green color component", str_c);
+	else if (!(rt->obj_cur->rgb.b >= 0 && rt->obj_cur->rgb.b <= 255))
+		kill_error("Wrong blue color component", str_c);
+}
+
 void object_parser(t_rt *rt, char *line, int str_c)
 {
 	while (*line && rt->read_b.str_c >= 1) {
@@ -97,7 +107,7 @@ void object_parser(t_rt *rt, char *line, int str_c)
 		}
 		else if (rt->read_b.str_c == 2 && (ft_strequ("rgb", rt->read_b.buff[1])) == 1 && *line == '{') {
 			line = take_rgb(line, &rt->obj_cur->rgb, str_c);
-			check_inti_rgb(rt, str_c);
+			check_inti_obj_rgb(rt, str_c);
 			rt->obj_cur->check[3] += 1;
 		}
 		else if (rt->read_b.str_c == 2 && (ft_strequ("radius", rt->read_b.buff[1])) == 1 && *line == '{') {
